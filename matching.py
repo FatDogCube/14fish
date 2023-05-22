f3fc862f0a286864f5d6fa1182b5dee94712e6d1
FatDogCube <djk.kleyn@gmail.com>
2023-05-22 20:55:49 +1000

    Add pixel matching

--- matching.py
+++ matching.py
@@ -22,11 +22,12 @@ def match(img_rgb, template, threshold=0.8):
         found_loc.add(pt)
         found = True
     points_to_remove = set()
+    # remove points that are very close
     for x1, y1 in found_loc:
         for x2, y2 in found_loc:
             if x1 == x2 and y1 == y2:
                 continue
-            if (x2 - x1) ** 2 + (y2 - y1) ** 2 < 9:
+            if (x2 - x1) ** 2 + (y2 - y1) ** 2 < 25:
                 if res[x1][y1] > res[x2][y2]:
                     points_to_remove.add((x2, y2))
                 else:
@@ -35,12 +36,36 @@ def match(img_rgb, template, threshold=0.8):
     return found, found_loc
 
 
+def match_pixel(img_rgb, px_loc, saturation, threshold):
+    pixel = img_rgb[px_loc[1]:px_loc[1] + 1, px_loc[0]:px_loc[0] + 1]
+    pixel_hsv = cv2.cvtColor(pixel, cv2.COLOR_BGR2HSV)
+    lower = int(saturation - threshold / 2)
+    upper = int(saturation + threshold / 2)
+    if lower <= pixel_hsv[0][0][1] <= upper:
+        return True, pixel_hsv[0][0][1]
+    else:
+        return False, pixel_hsv[0][0][1]
+
+
 def wait_for(img, template, timeout=10):
     found = False
     location = None
     starttime = time.time()
     while not found:
         found, location = match(img, template)
+        if found:
+            break
+        if time.time() > starttime + timeout:
+            print("timeout!")
+            break
+    return found, location
+
+
+def wait_for_pixel(img, px_loc, saturation, timeout):
+    found = False
+    starttime = time.time()
+    while not found:
+        found, location = match_pixel(img, px_loc, saturation, threshold=10)
         if found:
             break
         if time.time() > starttime + timeout:
